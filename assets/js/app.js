aa306fbc9d274bba0dfcfd349e5df736fcb26f34
usman <usman.khalid444@gmail.com>
2021-08-27 08:47:27 +0500

    make it better

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+/** @format */
+
+// contact us start
+document.querySelector("#contact-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  e.target.elements.name.value = "";
+  e.target.elements.email.value = "";
+  e.target.elements.message.value = "";
+});
+// contact us end
